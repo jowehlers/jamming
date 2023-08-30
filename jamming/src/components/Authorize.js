7ddf083cbb35785c6b9bdb7c2aa5d5abfb8d247c
DigitalFlowIO <135716608+DigitalFlowIO@users.noreply.github.com>
2023-08-30 12:18:02 -0400

    updated uri

--- jamming/src/components/Authorize.js
+++ jamming/src/components/Authorize.js
@@ -6,7 +6,7 @@ function Authorize() {
 
   const loginWithSpotify = () => {
     const client_id = "a6fe982e706643b2ae9d34a3a43b6d0e";
-    const redirect_uri = window.location.origin;
+    const redirect_uri = "https://jowehlers.github.io/jamming/";
     const scope = "user-read-private user-read-email";
 
     const authUrl = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${encodeURIComponent(redirect_uri)}&scope=${encodeURIComponent(scope)}&response_type=token`;
